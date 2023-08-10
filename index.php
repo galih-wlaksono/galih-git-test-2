dd2111af2f1d783b0ef0ebd2f499e18aacbf746a
Galih Widi Laksono <rahmanhadir955@gmail.com>
2023-08-10 14:27:50 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
 </head>
 <body>
     <h1>Hello World!</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus voluptatum quas expedita corporis quasi illo voluptates, possimus voluptatibus atque! Aliquam ullam omnis consequatur, aliquid sapiente recusandae laboriosam voluptates voluptas. Quae?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus voluptatum quas expedita corporis quasi illo voluptates, possimus voluptatibus atque! Aliquam ullam omnis consequatur, aliquid sapiente recusandae laboriosam voluptates voluptas. Quae?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
